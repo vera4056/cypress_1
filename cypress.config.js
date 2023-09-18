ddd30faed37dac20453846530db39d4b447a3c0f
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-09-18 22:57:30 +0300

    files modified

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 5000,
-  viewportWidth: 1000,
-  viewportHeight: 600,
-
-  component: {
-    viewportWidth: 500,
-    viewportHeight:500,
-  },
-
   e2e: {
     baseUrl:'http://localhost:3000',
-    //defaultCommandTimeout: 10000,
+    retries: {
+      runMode: 1,
+      openMode: 1,
+    },
+    viewportWidth: 1000,
+    viewportHeight: 660,
+
+    viewportWidth: 500,
+    viewportHeight: 500,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -31,6 +31,13 @@ describe('login page', () => {
       expect(el[0].validationMessage).to.be.eql('Заполните это поле.')
     })
   })
+
+describe('test', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
   it('add new book succesfully', () => {
     cy.login('test@test.com', 'test')
 
@@ -52,38 +59,41 @@ describe('login page', () => {
    cy.contains('Submit').click()
    })
 })
+
 it('delete from fav book succesfully', () => {
-  cy.login('test@test.com', 'test')
+   cy.login('test@test.com', 'test')
 
- cy.contains('Добро пожаловать test@test.com').should('be.visible')
- cy.contains('Log out').should('be.visible')
+   cy.contains('Добро пожаловать test@test.com').should('be.visible')
+   cy.contains('Log out').should('be.visible')
   
- cy.contains('Add new').click()
- cy.contains('Book description').should('be.visible')
+   cy.contains('Add new').click()
+   cy.contains('Book description').should('be.visible')
   
- cy.get('#title').type('Белый клык')
- cy.get('#description').type('Приключенческая повесть')
+   cy.get('#title').type('Белый клык')
+   cy.get('#description').type('Приключенческая повесть')
   
- cy.get('#authors').type('Джек Лондон')
- cy.get('#favorite').click()
+   cy.get('#authors').type('Джек Лондон')
+   cy.get('#favorite').click()
  
- cy.contains('Submit').click()
+   cy.contains('Submit').click()
 
- cy.contains('Delete from favorite').click()
+   cy.contains('Delete from favorite').click()
 
  })
 
-it.skip('add new book error on empty title', () => {
- cy.login('test@test.com', 'test')
+it('add new book error on empty title', () => {
+  cy.login('test@test.com', 'test')
 
- cy.contains('Добро пожаловать test@test.com').should('be.visible')
- cy.contains('Log out').should('be.visible')
+  cy.contains('Добро пожаловать test@test.com').should('be.visible')
+  cy.contains('Log out').should('be.visible')
   
- cy.contains('Add new').click()
- cy.contains('Book description').should('be.visible')
+  cy.contains('Add new').click()
+  cy.contains('Book description').should('be.visible')
   
- cy.get('#title').then((el) => {
+  cy.get('#title').then((el) => {
   expect(el[0].checkValidity()).to.be.false
   expect(el[0].validationMessage).to.be.eql('Заполните это поле.')
   })
+})
+
 })
